ab9c3a81d209975b2a075f37b29e623ed27b0604
eggplants <w10776e8w@yahoo.co.jp>
2021-08-15 12:01:28 +0900

    run linter

--- src/gion-gen.ts
+++ src/gion-gen.ts
@@ -5,22 +5,26 @@ const getRandomHiragana = (): string =>
 }
 const getRandomHiraganaSmall = (): string =>
 {
-	const hiraganaSmall = "ぁぃぅぇぉゕゖっゃゅょ"
+	// Const hiraganaSmall = "ぁぃぅぇぉゕゖっゃゅょ";
+	const hiraganaSmall = "っゃゅょ"
 	return hiraganaSmall[Math.floor(Math.random() * hiraganaSmall.length)]
 }
 
 export const gionGenerator =
 {
-	generate: (): string => {
-		if (Math.random() < 0.8) {
+	generate: (): string =>
+	{
+		if (Math.random() < 0.8)
+		{
 			// ABAB
-			return `${getRandomHiragana()}${getRandomHiragana()}`.repeat(2);
-		} else if (Math.random() < 0.5) {
-			// A'BA'B
-			return `${getRandomHiragana()}${getRandomHiraganaSmall()}${getRandomHiragana()}`.repeat(2);
-		} else {
-			// AB'AB'
-			return `${getRandomHiragana()}${getRandomHiragana()}${getRandomHiraganaSmall()}`.repeat(2);
+			return `${getRandomHiragana()}${getRandomHiragana()}`.repeat(2)
 		}
-	}
+		else if (Math.random() < 0.5)
+		{
+			// A'BA'B
+			return `${getRandomHiragana()}${getRandomHiraganaSmall()}${getRandomHiragana()}`.repeat(2)
+		}
+		// AB'AB'
+		return `${getRandomHiragana()}${getRandomHiragana()}${getRandomHiraganaSmall()}`.repeat(2)
+	},
 }
