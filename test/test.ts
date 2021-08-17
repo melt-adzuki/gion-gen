7dda792a514380bef2d559786c1e83fd827bcadc
hijiki02 <rebanira02@gmail.com>
2021-08-17 23:09:58 +0900

    ✅テストファイルを追加

---
+++ test/test.ts
@@ -0,0 +1,31 @@
+import { describe, it } from "mocha"
+import { expect } from "chai"
+import { gionGenerator } from "../src/gion-gen"
+
+const testCase = (...options: Array<[number, number]>): void => options.forEach(option =>
+{
+	it(`ケース${option[0]}の文字数が${option[1]}`, () =>
+	{
+		for (let i = 0; i <= 10; i++)
+		{
+			const gion = gionGenerator.generate(option[0])
+			console.log("%d: %s", i, gion)
+			expect(gion).to.be.lengthOf(option[1])
+		}
+	})
+})
+
+describe("ジェネレーターの出力", () =>
+{
+	it("空ではない", () =>
+	{
+		for (let i = 0; i <= 100; i++)
+		{
+			const gion = gionGenerator.generate()
+			console.log("%d: %s", i, gion)
+			expect(gion).not.to.equal("")
+		}
+	})
+
+	testCase([0, 6], [1, 6], [2, 5], [3, 5], [4, 7], [5, 8])
+})
