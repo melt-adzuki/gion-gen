c04143a6eb54f6abd00a3684244890d64bfbd2a0
あずき⪥™ <44765629+hijiki02@users.noreply.github.com>
2021-09-19 09:49:22 +0900

    🔧srcディレクトリへのエイリアスを設定

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,34 @@
 import { loadEnv } from "vite"
+import path from "path"
 
-export default ({ command, mode }) =>
+const specificOptions = ({ command, mode }) =>
 {
 	if (command === "serve")
 	{
 		return {
 			// Serve specific config
 			server: {
+				fs: {
+					allow: ["."],
+				},
 				host: loadEnv(mode, process.cwd()).VITE_SERVER_IP,
 			},
 		}
 	}
-
 	return {
 		base: "/GION/",
 	}
 }
+
+export default ({ command, mode }) => ({ ...specificOptions({ command,
+	mode }),
+...{
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: path.resolve(__dirname, "src"),
+			},
+		],
+	},
+} })
