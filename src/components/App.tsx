5648e76bd23d152c9bc01ed859984f8566d457da
あずき⪥™ <44765629+hijiki02@users.noreply.github.com>
2021-10-29 19:15:50 +0900

    ✨滑らかなグラデーション背景

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,46 +1,72 @@
 import "modern-css-reset"
-import styled, { keyframes } from "styled-components"
 import Arrow from "./Arrow"
 import Card from "./Card"
 import Control from "./Control"
+import Granim from "granim"
+import React from "react"
 import Result from "./Result"
+import styled from "styled-components"
 
-const gradient = keyframes`
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-	}
-`
 
-const Container = styled.section`
-	font-family: "RocknRoll One", sans-serif;
-	width: 100vw;
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 32px;
-	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-	background-size: 400% 400%;
-	animation: ${gradient} 15s ease infinite;
-`
-
-const App = (): JSX.Element =>
+class App extends React.Component
 {
-	return (
-		<Container>
-			<Arrow />
-			<Card />
-			<Result />
-			<Control />
-		</Container>
-	)
+	private granimInstance!: Granim
+
+	private Container = styled.section`
+		font-family: "RocknRoll One", sans-serif;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 32px;
+	`
+
+	private GranimCanvas = styled.canvas`
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: -1;
+	`
+
+	render(): JSX.Element
+	{
+		const { Container, GranimCanvas } = this
+
+		return (
+			<>
+				<GranimCanvas id="granim-canvas" />
+				<Container>
+					<Arrow />
+					<Card />
+					<Result />
+					<Control />
+				</Container>
+			</>
+		)
+	}
+
+	componentDidMount(): void
+	{
+		this.granimInstance = new Granim({
+			direction: "left-right",
+			element: "#granim-canvas",
+			states:
+			{
+				"default-state":
+				{
+					gradients: [["#FF8008", "#be9c46"], ["#4CB8C4", "#3CD3AD"], ["#24C6DC", "#514A9D"], ["#b045be", "#912323"]],
+					transitionSpeed: 2000,
+				},
+			},
+		})
+	}
 }
+
 
 export default App
