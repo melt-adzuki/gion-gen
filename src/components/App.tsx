0c988ef35caefc011e6c508a891485fd3dda5e55
あずき⪥™ <44765629+melt-adzuki@users.noreply.github.com>
2022-08-02 16:46:12 +0900

    refactor(App): prefer class to function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,75 +5,50 @@ import Card from "./Card"
 import Control from "./Control"
 import Granim from "granim"
 import Header from "./Header"
-import React from "react"
 import Result from "./Result"
 import SettingsPanel from "./SettingsPanel"
 import media from "styled-media-query"
 import styled from "styled-components"
+import { useEffect } from "react"
 
+const Grid = styled.section`
+	font-family: "RocknRoll One", sans-serif;
+	width: 100vw;
+	height: 100vh;
+	display: grid;
+	grid-template-columns: 1fr minmax(0, 2fr) 1fr;
+	grid-template-rows: minmax(0, 1fr) 2fr minmax(0, 1fr);
+	grid-column-gap: 0px;
+	grid-row-gap: 0px;
+	align-items: center;
+	justify-content: center;
+	padding: 32px;
+`
 
-class App extends React.Component
+const Center = styled.div`
+	grid-area: 2 / 2 / 3 / 3;
+	${media.lessThan("huge")`
+		grid-area: 2 / 1 / 3 / 4;
+	`}
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+`
+
+const Top = styled.div`
+	grid-area: 1 / 1 / 2 / 4;
+	display: flex;
+	flex-direction: column;
+	gap: 32px;
+	height: 100%;
+`
+
+const App = (): JSX.Element =>
 {
-	private granimInstance?: Granim
-
-	private readonly Grid = styled.section`
-		font-family: "RocknRoll One", sans-serif;
-		width: 100vw;
-		height: 100vh;
-		display: grid;
-		grid-template-columns: 1fr minmax(0, 2fr) 1fr;
-		grid-template-rows: minmax(0, 1fr) 2fr minmax(0, 1fr);
-		grid-column-gap: 0px;
-		grid-row-gap: 0px;
-		align-items: center;
-		justify-content: center;
-		padding: 32px;
-	`
-
-	private readonly Center = styled.div`
-		grid-area: 2 / 2 / 3 / 3;
-		${media.lessThan("huge")`
-			grid-area: 2 / 1 / 3 / 4;
-		`}
-		display: flex;
-		flex-direction: column;
-		gap: 16px;
-	`
-
-	private readonly Top = styled.div`
-		grid-area: 1 / 1 / 2 / 4;
-		display: flex;
-		flex-direction: column;
-		gap: 32px;
-		height: 100%;
-	`
-
-	render(): JSX.Element
+	useEffect(() =>
 	{
-		const { Grid, Center, Top } = this
-
-		return (
-			<>
-				<canvas className="stack" id="granim-canvas" />
-				<Grid className="stack">
-					<Top>
-						<Header />
-						<Card />
-					</Top>
-					<Center>
-						<Result />
-						<Control />
-					</Center>
-					<Arrow />
-				</Grid>
-				<SettingsPanel />
-			</>
-		)
-	}
-
-	componentDidMount(): void
-	{
-		this.granimInstance = new Granim({
+		// eslint-disable-next-line no-new
+		new Granim({
 			direction: "left-right",
 			element: "#granim-canvas",
 			states:
@@ -85,7 +60,25 @@ class App extends React.Component
 				},
 			},
 		})
-	}
+	}, [])
+
+	return (
+		<>
+			<canvas className="stack" id="granim-canvas" />
+			<Grid className="stack">
+				<Top>
+					<Header />
+					<Card />
+				</Top>
+				<Center>
+					<Result />
+					<Control />
+				</Center>
+				<Arrow />
+			</Grid>
+			<SettingsPanel />
+		</>
+	)
 }
 
 
